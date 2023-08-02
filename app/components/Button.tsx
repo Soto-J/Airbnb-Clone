ce6c84d21fac7947b9ce3d578267f6dc5243fd7c
John Soto <soto.ja415@gmail.com>
2023-08-02 13:09:55 -0400

    Added empty state

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -25,21 +25,21 @@ const Button: React.FC<ButtonProps> = ({
       onClick={onClick}
       disabled={disabled}
       className={`
-        relative
-        w-full
-        rounded-lg
-        transition
-        hover:opacity-80
-        disabled:cursor-not-allowed
-        disabled:opacity-70
-        ${outline ? "bg-white" : "bg-rose-500"}
-        ${outline ? "border-black" : "border-rose-500"}
-        ${outline ? "text-black" : "text-white"}
-        
-        ${small ? "py-1" : "py-3"}
-        ${small ? "font-light" : "font-semibold"}
-        ${small ? "text-sm" : "text-md"}
-        ${small ? "border-[1px]" : "border-[2px]"}
+      relative
+      w-full
+      rounded-lg
+      transition
+      hover:opacity-80
+      disabled:cursor-not-allowed
+      disabled:opacity-70
+      
+      ${outline ? "bg-white" : "bg-rose-500"}
+      ${outline ? "border-black" : "border-rose-500"}
+      ${outline ? "text-black" : "text-white"}
+      ${small ? "text-sm" : "text-md"}
+      ${small ? "py-1" : "py-3"}
+      ${small ? "font-light" : "font-semibold"}
+      ${small ? "border-[1px]" : "border-2"}
       `}
     >
       {Icon && <Icon size={24} className="absolute left-4 top-3" />}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
 import ClientOnly from "./components/ClientOnly";
 import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
 
 export default function Home() {
+  const isEmpty = true;
+
+  if (isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    );
+  }
+
   return (
     <ClientOnly>
       <Container>
@@ -9,13 +20,13 @@ export default function Home() {
           className="
             grid
             grid-cols-1
+            gap-8
             pt-24
             sm:grid-cols-2
             md:grid-cols-3
             lg:grid-cols-4
             xl:grid-cols-5
-            2xl:grid-cols-6
-            gap-8  
+            2xl:grid-cols-6  
           "
         >
           <div>My Future Listings</div>
