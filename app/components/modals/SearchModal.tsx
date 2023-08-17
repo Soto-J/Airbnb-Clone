ef4c36a499964606ff9e30db931d7ef0a6c919b3
John Soto <soto.ja415@gmail.com>
2023-08-16 20:32:01 -0400

    Added steps to modal

--- app/components/modals/SearchModal.tsx
+++ app/components/modals/SearchModal.tsx
@@ -2,15 +2,17 @@
 import { useCallback, useMemo, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { Range } from "react-date-range";
+import { formatISO } from "date-fns";
 import qs from "query-string";
 
 import useSearchModal from "@/app/hooks/useSearchModal";
-import { CountrySelectValue } from "../inputs/CountrySelect";
 
+import CountrySelect, { CountrySelectValue } from "../inputs/CountrySelect";
 import Modal from "./Modal";
 import dynamic from "next/dynamic";
-import { formatISO } from "date-fns";
 import Heading from "../Heading";
+import Calendar from "../inputs/Calendar";
+import Counter from "../inputs/Counter";
 
 enum STEPS {
   LOCATION = 0,
@@ -19,8 +21,8 @@ enum STEPS {
 }
 
 const SearchModal = () => {
-  const [location, setLocation] = useState<CountrySelectValue>();
   const [step, setStep] = useState(STEPS.LOCATION);
+  const [location, setLocation] = useState<CountrySelectValue>();
   const [guestCount, setGuestCount] = useState(1);
   const [roomCount, setRoomCount] = useState(1);
   const [bathroomCount, setBathroomCount] = useState(1);
@@ -34,7 +36,7 @@ const SearchModal = () => {
   const params = useSearchParams();
   const searchModal = useSearchModal();
 
-  const map = useMemo(
+  const Map = useMemo(
     () =>
       dynamic(() => import("../Map"), {
         ssr: false,
@@ -109,13 +111,20 @@ const SearchModal = () => {
   ]);
 
   const actionLabel = useMemo(() => {
-    // INFO === last step
-    return step === STEPS.INFO ? "Search" : "Next";
+    if (step === STEPS.INFO) {
+      return "Search";
+    }
+
+    return "Next";
   }, [step]);
 
   const secondaryActionLabel = useMemo(() => {
     // LOCATION === first step
-    return step === STEPS.LOCATION && "Back";
+    if (step === STEPS.LOCATION) {
+      return undefined;
+    }
+
+    return "back";
   }, [step]);
 
   let bodyContent = (
@@ -124,16 +133,75 @@ const SearchModal = () => {
         title="Where do you want to go?"
         subtitle="Find the perfect location"
       />
+
+      <CountrySelect
+        value={location}
+        onChange={(value) => setLocation(value as CountrySelectValue)}
+      />
+      <hr />
+
+      <Map center={location?.latlng} />
     </div>
   );
+
+  if (step === STEPS.DATE) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading
+          title="When do you plan to go?"
+          subtitle="Make sure everyone is free!"
+        />
+
+        <Calendar
+          value={dateRange}
+          disabledDates={[]}
+          onChange={(value) => setDateRange(value.selection)}
+        />
+      </div>
+    );
+  }
+
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading title="More information" subtitle="Find your perfect place!" />
+
+        <Counter
+          title="Guests"
+          subtitle="How many guests are coming?"
+          value={guestCount}
+          onChange={(value) => setGuestCount(value)}
+        />
+        <hr />
+
+        <Counter
+          title="Rooms"
+          subtitle="How many rooms?"
+          value={roomCount}
+          onChange={(value) => setRoomCount(value)}
+        />
+        <hr />
+
+        <Counter
+          title="Bathrooms"
+          subtitle="How many bathroom?"
+          value={bathroomCount}
+          onChange={(value) => setBathroomCount(value)}
+        />
+      </div>
+    );
+  }
 
   return (
     <Modal
       isOpen={searchModal.isOpen}
-      onSubmit={searchModal.onOpen}
+      onSubmit={onSubmit}
       onClose={searchModal.onClose}
-      title="Filters"
-      actionLabel="Search"
+      title={"Filters"}
+      actionLabel={actionLabel}
+      secondaryActionLabel={secondaryActionLabel}
+      secondaryAction={step === STEPS.LOCATION ? undefined : onBack}
+      body={bodyContent}
     />
   );
 };
