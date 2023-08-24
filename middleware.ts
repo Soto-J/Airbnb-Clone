2b4d93686bdc013e27559eece712cdbc197ab839
John Soto <soto.ja415@gmail.com>
2023-08-24 03:12:35 -0400

    Fixed middleware bug

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,10 @@
-export { default } from "next-auth/middleware";
+// export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+
+// redirect to home page if not authenticated
+export default withAuth({
+  secret: process.env.SECRET,
+});
 
 export const config = {
   matcher: ["/trips", "/reservations", "/properties", "/favorites"],
